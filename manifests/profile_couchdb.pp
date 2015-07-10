6c17d1545260c0fd0cd9908afce54d89e5c8611d
Marc Lambrichs <mlambrichs@melange-it.nl>
2015-07-10 14:35:46 +0200

    Added couchdb profile

---
+++ manifests/profile_couchdb.pp
@@ -0,0 +1,11 @@
+class arthurjames::profile_couchdb (
+){
+  include couchdb
+  couchdb::instance { 'main':
+    port           => '5985'
+    www_auth       => true,
+    admin_login    => 'admin',
+    admin_password => 'admin'
+  }
+  couchdb::db { 'test': }
+}
