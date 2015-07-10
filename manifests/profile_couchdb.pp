81dd4b897599a7ffd48b302d4a524c0a3fb6e92d
Marc Lambrichs <mlambrichs@melange-it.nl>
2015-07-10 14:39:01 +0200

    Added missing ,. Typo

--- manifests/profile_couchdb.pp
+++ manifests/profile_couchdb.pp
@@ -2,7 +2,7 @@ class arthurjames::profile_couchdb (
 ){
   include couchdb
   couchdb::instance { 'main':
-    port           => '5985'
+    port           => '5985',
     www_auth       => true,
     admin_login    => 'admin',
     admin_password => 'admin'
