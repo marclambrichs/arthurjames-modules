fc92526395ddea7d4ba9b659c1b62b485231b45f
Marc Lambrichs <mlambrichs@melange-it.nl>
2015-10-29 15:10:19 +0100

    Revert to e13c34173c56c8e0517740f957217c7bf00efd50

--- manifests/profile_puppetdb.pp
+++ manifests/profile_puppetdb.pp
@@ -1,13 +1,17 @@
 class arthurjames::profile_puppetdb (
   $listen_address     = '0.0.0.0',
+  $manage_dbserver    = true,
   $manage_firewall    = false,
-  $ssl_listen_address = '0.0.0.0'
+  $ssl_listen_address = '0.0.0.0',
+  $puppetdb_version   = '2.3.8'
 ){
 
   # Configure puppetdb and its underlying database
   class { '::puppetdb':
     listen_address     => $listen_address,
+    manage_dbserver    => $manage_dbserver,
     manage_firewall    => $manage_firewall,
-    ssl_listen_address => $ssl_listen_address
+    ssl_listen_address => $ssl_listen_address,
+    puppetdb_version   => $puppetdb_version
   }
 }
