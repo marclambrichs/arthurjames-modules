e86acc595a02ed08aa62cd006b8c8b90cb7433af
Marc Lambrichs <mlambrichs@melange-it.nl>
2015-10-29 17:31:47 +0100

    Removed unnecessary variables

--- manifests/profile_puppetdb.pp
+++ manifests/profile_puppetdb.pp
@@ -1,8 +1,4 @@
 class arthurjames::profile_puppetdb (
-  $listen_address     = '0.0.0.0',
-  $manage_dbserver    = true,
-  $manage_firewall    = false,
-  $ssl_listen_address = '0.0.0.0',
   $puppetdb_version   = '2.3.8-1puppetlabs1'
 ){
 
