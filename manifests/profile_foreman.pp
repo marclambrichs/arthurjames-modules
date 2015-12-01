eab513fc9fe1bcb3ba35505bc1b97acee4a38b84
Marc Lambrichs <mlambrichs@melange-it.nl>
2015-12-01 10:43:55 +0100

    enabled ssl for foreman

--- manifests/profile_foreman.pp
+++ manifests/profile_foreman.pp
@@ -2,25 +2,35 @@ class arthurjames::profile_foreman (
   $foreman_admin_password = 'changeme',
   $foreman_host           = $::fqdn,
   $foreman_repo           = 'stable',
-  $ssl                    = false
+  $ssl                    = true,
+  $server_ssl_ca          = '/var/lib/puppet/ssl/certs/ca.pem',
+  $server_ssl_chain       = '/var/lib/puppet/ssl/certs/ca.pem',
+  $server_ssl_cert        = '/var/lib/puppet/ssl/certs/${::fqdn}.pem',
+  $server_ssl_key         = '/var/lib/puppet/ssl/private_keys/${::fqdn}.pem',
+  $server_ssl_crl         = '/var/lib/puppet/ssl/ca/ca_crl.pem',
 ){
 
   class { '::foreman':
-    admin_password => $foreman_admin_password,
-    apipie_task    => 'apipie:cache:index',
+    admin_password   => $foreman_admin_password,
+    apipie_task      => 'apipie:cache:index',
     # Enable users authentication (default user:admin pw:changeme)
-    authentication => true,
-    db_manage      => true,
+    authentication   => true,
+    db_manage        => true,
     # configure foreman via apache and passenger
-    foreman_url    => "https://${foreman_host}",
-    passenger      => true,
-    repo           => $foreman_repo,
-    selinux        => false,
-    ssl            => $ssl,
+    foreman_url      => "https://${foreman_host}",
+    passenger        => true,
+    repo             => $foreman_repo,
+    selinux          => false,
+    ssl              => $ssl,
+    server_ssl_ca    => $server_ssl_ca,
+    server_ssl_chain => $server_ssl_chain,
+    server_ssl_cert  => $server_ssl_cert,
+    server_ssl_key   => $server_ssl_key,
+    server_ssl_crl   => $server_ssl_crl,
     # should foreman manage host provisioning as well
-    unattended     => true,
+    unattended       => true,
     # Enclose apache configuration in <VirtualHost>...</VirtualHost>
-    use_vhost      => true
+    use_vhost        => true
   }
 
   class { '::foreman_proxy':
