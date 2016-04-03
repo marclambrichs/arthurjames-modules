82a1860595654119bcef19d3e186d16887c8564d
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-04-03 14:51:01 +0200

    Added variable for enabling memcached

--- manifests/role_graphite.pp
+++ manifests/role_graphite.pp
@@ -2,11 +2,13 @@
 class arthurjames::role_graphite (
   $grafana_enabled  = false,
   $graphite_enabled = false,
+  $memcached_enabled = false,
   $statsd_enabled   = false
 ){
   validate_bool(
     $grafana_enabled,
     $graphite_enabled,
+    $memcached_enabled,
     $statsd_enabled
   )
 
@@ -16,10 +18,18 @@ class arthurjames::role_graphite (
   }
   if $graphite_enabled {
     notify { 'graphite enabled': }
-    include ::arthurjames::profile_graphite
+    class { 'arthurjames::profile_graphite':
+      memcached_enabled => $memcached_enabled
+    }
+  }
+  if $memcached_enabled {
+    notify { 'memcached enabled': }
+    include ::arthurjames::profile_memcached
   }
   if $statsd_enabled {
     notify { 'statsd enabled': }
     include ::arthurjames::profile_statsd
   }
+
+  Class['::arthurjames::profile_memcached'] -> Class['::arthurjames::profile_graphite']
 }
