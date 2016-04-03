5cae5615c9df439b9db8cfde7b0973d478e260e0
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-04-03 16:10:34 +0200

    puppet-lint

--- manifests/profile_grafana.pp
+++ manifests/profile_grafana.pp
@@ -5,16 +5,27 @@ class arthurjames::profile_grafana {
 
   class { 'grafana':
     install_method => 'repo',
-    cfg => {
-      server       => {
+    cfg            => {
+      server   => {
         http_addr => '127.0.0.1',
       },
+      database => {
+        'type'   => 'sqlite3',
+        host     => '127.0.0.1:3306',
+        name     => 'grafana',
+        user     => 'root',
+        password => '',
+      },
+      users    => {
+        allow_sign_up => false,
+      },
     },
+
   }
 
   apache::vhost { 'grafana.test.vagrant':
-    port            => '80',
-    docroot         => '/var/www',
-    proxy_dest      => 'http://127.0.0.1:3000',
+    port       => '80',
+    docroot    => '/var/www',
+    proxy_dest => 'http://127.0.0.1:3000',
   }
 }
