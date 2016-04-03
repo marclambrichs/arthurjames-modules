81e012adb4dcecbba0ab097ddfbe13516f03b1de
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-04-03 23:33:19 +0200

    grafana configuration

--- manifests/profile_grafana.pp
+++ manifests/profile_grafana.pp
@@ -6,24 +6,25 @@ class arthurjames::profile_grafana {
   class { 'grafana':
     install_method => 'repo',
     cfg            => {
-      server   => {
+      server           => {
         http_addr => '127.0.0.1',
       },
-      database => {
-        'type'   => 'sqlite3',
-        host     => '127.0.0.1:3306',
-        name     => 'grafana',
-        user     => 'root',
-        password => '',
+      database         => {
+        'type' => 'sqlite3',
+        path   => '/var/lib/grafana',
+        name   => 'grafana',
       },
-      users    => {
+      users            => {
         allow_sign_up => false,
       },
+      'auth.anonymous' => {
+        enabled => true
+      }
     },
 
   }
 
-  apache::vhost { 'grafana.test.vagrant':
+  apache::vhost { 'grafana.arthurjames.vagrant':
     port       => '80',
     docroot    => '/var/www',
     proxy_dest => 'http://127.0.0.1:3000',
