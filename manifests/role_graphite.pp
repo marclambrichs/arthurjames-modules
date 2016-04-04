80e896404779de1b253a1ef90a00ae9658f8b0e6
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-04-04 18:36:06 +0200

    Removed notifies

--- manifests/role_graphite.pp
+++ manifests/role_graphite.pp
@@ -13,21 +13,17 @@ class arthurjames::role_graphite (
   )
 
   if $grafana_enabled {
-    notify { 'grafana enabled': }
     include ::arthurjames::profile_grafana
   }
   if $graphite_enabled {
-    notify { 'graphite enabled': }
     class { 'arthurjames::profile_graphite':
       memcached_enabled => $memcached_enabled
     }
   }
   if $memcached_enabled {
-    notify { 'memcached enabled': }
     include ::arthurjames::profile_memcached
   }
   if $statsd_enabled {
-    notify { 'statsd enabled': }
     include ::arthurjames::profile_statsd
   }
 
