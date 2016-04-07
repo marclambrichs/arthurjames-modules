8c1e4077b18a6f1be93066e9e90f038f70edd217
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-04-07 07:03:26 +0200

    Reorganized variables

--- manifests/profile_grafana.pp
+++ manifests/profile_grafana.pp
@@ -15,11 +15,35 @@
 #-# Expired days of log file(delete after max days), default is 7
 #-;max_days = 7
 class arthurjames::profile_grafana (
-  $logmode = 'console, file'
+
+  $allow_sign_up               = true,
+  $allow_org_create            = false,
+  $auto_assign_org             = true,
+  $auth_assign_org_role        = 'Viewer'
+  $admin_password              = 'changeme',
+  $admin_user                  = 'admin',
+  $cookie_username             = 'grafana_user',
+  $cookie_remember_name        = 'grafana_remember',
+  $data_source_proxy_whitelist = '',
+  $db_path                     = '/var/lib/grafana/grafana.db',
+  $db_name                     = 'grafana',
+  $db_type                     = 'sqlite3',
+  $disable_gravatar            = false,
+  $install_method              = 'repo',
+  $log_buffer_length           = 10000,
+  $log_rotate                  = true,
+  $log_max_lines               = 1000000,
+  $log_max_lines_shift         = 28,
+  $log_daily_rotate            = true,
+  $login_remember_days         = 7,
+  $log_max_days                = 7,
+  $loglevel                    = 'Info',
+  $logmode                     = 'console, file'
+  $secret_key                  = 'inWSYLbKCoLko',
 ){
 
   class { 'grafana':
-    install_method => 'repo',
+    install_method => $install_method,
     cfg            => {
       server            => {
         protocol  => 'http',
@@ -49,40 +73,40 @@ class arthurjames::profile_grafana (
         path    => '/var/lib/grafana/dashboards',
       },
       database          => {
-        'type' => 'sqlite3',
-        path   => '/var/lib/grafana/grafana.db',
-        name   => 'grafana',
+        'type' => $db_type
+        path   => $db_path,
+        name   => $db_name,
       },
       log               => {
         mode       => $logmode,
-        buffer_len => 10000,
-        level      => 'Info',
+        buffer_len => $log_buffer_length,
+        level      => $loglevel,
       },
       'log.console'     => {
         level => '',
       },
       'log.file'        => {
-        log_rotate      => true,
-        max_lines       => 1000000,
-        max_lines_shift => 28,
-        daily_rotate    => true,
-        max_days        => 7,
+        log_rotate      => $log_rotate,
+        max_lines       => $log_max_lines,
+        max_lines_shift => $log_max_lines_shift,
+        daily_rotate    => $log_daily_rotate,
+        max_days        => $log_max_days,
       },
       security          => {
-        admin_user                  => 'admin',
-        admin_password              => 'changeme',
-        secret_key                  => 'inWSYLbKCoLko',
-        login_remember_days         => 7,
-        cookie_username             => 'grafana_user',
-        cookie_remember_name        => 'grafana_remember',
-        disable_gravatar            => false,
-        data_source_proxy_whitelist => '',
+        admin_user                  => $admin_user,
+        admin_password              => $admin_password,
+        secret_key                  => $secret_key,
+        login_remember_days         => $login_remember_days,
+        cookie_username             => $cookie_username,
+        cookie_remember_name        => $cookie_remember_name,
+        disable_gravatar            => $disable_gravatar,
+        data_source_proxy_whitelist => $data_source_proxy_whitelist,
       },
       users             => {
-        allow_sign_up        => true,
-        allow_org_create     => false,
-        auto_assign_org      => true,
-        auth_assign_org_role => 'Viewer'
+        allow_sign_up        => $allow_sign_up,
+        allow_org_create     => $allow_org_create,
+        auto_assign_org      => $auto_assign_org,
+        auth_assign_org_role => $auth_assign_org_role,
       },
 
     },
