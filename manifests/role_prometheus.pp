76c8573342e90052ca0712241dc6d051db5f0a5d
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-05-02 07:20:06 +0200

    Added grafana to prometheus node

--- manifests/role_prometheus.pp
+++ manifests/role_prometheus.pp
@@ -1,6 +1,20 @@
 # == class arthurjames::role_prometheus
 #
 #
-class arthurjames::role_prometheus {
-  include ::arthurjames::profile_prometheus
+class arthurjames::role_prometheus (
+  $grafana_enabled    = false,
+  $prometheus_enabled = false,
+) {
+
+  validate_bool(
+   $grafana_enabled,
+   $prometheus_enabled,
+  )
+  
+  if $grafana_enabled {
+    include ::arthurjames::profile_grafana
+  }
+  if $prometheus_enabled {
+    include ::arthurjames::profile_prometheus
+  }
 }
