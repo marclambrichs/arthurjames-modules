4d30103a9857d97396265692c0483e083e7f2d4b
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-08-09 20:36:44 +0200

    removed collectd from role_neo4j

--- manifests/role_neo4j.pp
+++ manifests/role_neo4j.pp
@@ -1,14 +1,8 @@
 class arthurjames::role_neo4j (
-  $collectd_enabled = false,
   $neo4j_enabled   = false,
 ){
-  validate_bool( $collectd_enabled )
   validate_bool( $neo4j_enabled )
 
-  if $collectd_enabled {
-    notify { 'collectd enabled': }
-    include ::arthurjames::profile_collectd
-  }
   if $neo4j_enabled {
     notify { 'neo4j enabled': }
     include ::arthurjames::profile_neo4j
